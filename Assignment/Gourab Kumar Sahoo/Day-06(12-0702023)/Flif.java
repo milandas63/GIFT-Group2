50e41909137f4b56bd501256ae52c58755138106
Srbidhar <138988065+Srbidhar@users.noreply.github.com>
2023-07-13 14:04:04 +0530

    Update Flif.java

--- Assignment/Gourab Kumar Sahoo/Day-06(12-0702023)/Flif.java
+++ Assignment/Gourab Kumar Sahoo/Day-06(12-0702023)/Flif.java
@@ -1,3 +1,7 @@
+/*Flip/Invert/Toggle the case of all characters in a given String?
+        Original:   Quick Brown Fox Jumps Over The Lazy Dog
+        Invert:     qUICK bROWN fOX jUMPS oVER tHE lAZY dOG   */
+
 public class CaseFlip {
     public static void main(String[] args) {
         String original = "Quick Brown Fox Jumps Over The Lazy Dog";
