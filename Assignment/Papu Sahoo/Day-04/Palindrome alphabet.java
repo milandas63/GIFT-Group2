5bfb66b8f7f9398f44e39b957de044a35756da94
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-13 21:10:51 +0530

    Update Palindrome alphabet.java

--- Assignment/Papu Sahoo/Day-04/Palindrome alphabet.java
+++ Assignment/Papu Sahoo/Day-04/Palindrome alphabet.java
@@ -1,1 +1,26 @@
+/*Q:- Draw a triangle of palindrome alphabets pyramid  */
+public class PalindromeTriangle {
+    public static void main(String[] args) {
+        int rows = 5; 
+         for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char)('A' + j) + " ");
+            }
+            for (int j = i - 1; j >= 0; j--) {
+                System.out.print((char)('A' + j) + " ");
+            }
+              System.out.println(); // Move to the next line
+        }
+    }
+}
 
+Output:
+
+          a 
+        a b a 
+      a b c b a 
+    a b c d c b a 
+  a b c d e d c b a    
