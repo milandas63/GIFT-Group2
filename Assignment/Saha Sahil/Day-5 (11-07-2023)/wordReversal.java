3be52e319703d5f336d6709269797df075d67de5
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-13 22:49:07 +0530

    Create wordReversal.java

---
+++ Assignment/Saha Sahil/Day-5 (11-07-2023)/wordReversal.java
@@ -0,0 +1,17 @@
+public class WordReversal {
+    public static void main(String[] args) {
+        String input = "Hello, World! How are you?";
+        String reversed = reverseWords(input);
+        System.out.println("Original string: " + input);
+        System.out.println("Reversed words: " + reversed);
+    }
+
+    public static String reverseWords(String input) {
+        String[] words = input.split(" ");
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]).append(" ");
+        }
+        return reversed.toString().trim();
+    }
+}
