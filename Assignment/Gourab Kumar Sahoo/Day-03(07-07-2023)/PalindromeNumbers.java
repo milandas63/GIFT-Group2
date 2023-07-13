48c50e1b3a17dbe145a1fbbaf2c6b25f414abca3
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-13 09:30:30 +0530

    Update PalindromeNumbers.java

--- Assignment/Gourab Kumar Sahoo/Day-03(07-07-2023)/PalindromeNumbers.java
+++ Assignment/Gourab Kumar Sahoo/Day-03(07-07-2023)/PalindromeNumbers.java
@@ -1,1 +1,29 @@
+/*
+	c)  Draw a triangle of palindrome numbers?
+					        1        
+					       121       
+					      12321      
+					     1234321     
+					    123454321    
+					   12345654321   
+					  1234567654321  
+					 123456787654321 
+					12345678987654321
+ */
 
+
+public class Pyramid2 {
+
+	public static void main(String[] args) {
+		int spaces = 40;
+		
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=1; j<=i; j++)      System.out.print(j);
+			for(int j=(i-1); j>0; j--)   System.out.print(j);
+			System.out.println();
+			spaces--;
+		}
+	}
+
+}
