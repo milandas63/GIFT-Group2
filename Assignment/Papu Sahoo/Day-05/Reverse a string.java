8c41ff45c16c197d5bd07214808e7a82274c2d87
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-13 23:07:41 +0530

    Update Reverse a string.java

--- Assignment/Papu Sahoo/Day-05/Reverse a string.java
+++ Assignment/Papu Sahoo/Day-05/Reverse a string.java
@@ -1,1 +1,18 @@
-
+  // Write a program to reverse a String
+	 Example: Gandhi Institute For Technology //
+	
+	public class ReserveString {
+	public static void main(String[] args) {
+		String text = "Gandhi Institute For Technology";
+		String reverse = "";
+		char[] alphas = text.toCharArray();
+		for(int i=alphas.length-1; i>=0; i--) {
+			reverse = reverse+alphas[i];
+		}
+		System.out.println("Original: "+text);
+		System.out.println("Reverse:  "+reverse);
+System.out.println("----------------------------");
+  }
+}
+Output:
+  Result:  ygolonhceT roF etutitsnI ihdnaG 
