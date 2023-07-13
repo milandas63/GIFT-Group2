76d3babe210279cd8cf1783d5c73f2d1b4b61bad
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-13 09:28:17 +0530

    Update Asterisks.java

--- Assignment/Gourab Kumar Sahoo/Day-03(07-07-2023)/Asterisks.java
+++ Assignment/Gourab Kumar Sahoo/Day-03(07-07-2023)/Asterisks.java
@@ -1,1 +1,31 @@
+package com.algorithm;
 
+/*
+	a)  Draw a triangle of asterisks?
+			         *
+			        ***
+			       *****
+			      *******
+			     *********
+			    ***********
+			   *************
+			  ***************
+			 *****************
+ */
+
+public class Pyramid1 {
+
+	public static void main(String[] args) {
+		int spaces = 40;
+		int stars = 1;
+		
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=1; j<=stars; j++)  System.out.print("*");
+			System.out.println();
+			spaces--; stars+=2;
+		}
+		
+	}
+
+}
