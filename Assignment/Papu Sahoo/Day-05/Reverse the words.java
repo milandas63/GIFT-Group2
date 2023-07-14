8f61b7f8eb51fab59dcf17434779736a88e3c253
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-14 07:51:09 +0530

    Update Reverse the words.java

--- Assignment/Papu Sahoo/Day-05/Reverse the words.java
+++ Assignment/Papu Sahoo/Day-05/Reverse the words.java
@@ -1,1 +1,18 @@
-
+ // Write a program to reverse the words in a given String?
+	 Example: God is Omnipotent and Omniscient //
+	
+	public class ReserveString {
+	public static void main(String[] args) {
+		text = "God is Omnipotent and Omniscient";
+		reverse = "";
+		String[] words = text.split(" ");
+		for(int i=words.length-1; i>=0; i--) {
+			reverse = reverse + words[i]+ " ";
+		}
+		System.out.println("Original: "+text);
+		System.out.println("Reverse:  "+reverse);
+    System.out.println("-----------------------------------");
+	}
+}
+Output:
+      Omniscient and Omnipotent is God 
