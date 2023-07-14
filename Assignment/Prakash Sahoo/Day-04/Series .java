061bafa43bbbeb5310380c60ab970a9d19682592
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-14 09:22:49 +0530

    Update Series .java

--- Assignment/Prakash Sahoo/Day-04/Series .java
+++ Assignment/Prakash Sahoo/Day-04/Series .java
@@ -1,1 +1,15 @@
-
+public class SeriesCompletion {
+    public static void main(String[] args) {
+        int n = 21;
+         for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                n = n * 2 + 1;
+            } else {
+                n = n * 2 - 1;
+            }
+            System.out.print(n + " ");
+        }
+    }
+}
+Output:
+43 85 171 341 683 1365 2731 5461 10923 21845 
