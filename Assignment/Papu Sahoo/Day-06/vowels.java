6ed211452ee382db5c79a6293da801e2a238cd92
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-14 08:12:01 +0530

    Update vowels.java

--- Assignment/Papu Sahoo/Day-06/vowels.java
+++ Assignment/Papu Sahoo/Day-06/vowels.java
@@ -1,1 +1,21 @@
-
+//Count the number of vowels in a given string //
+public class VowelCount {
+    public static void main(String[] args) {
+        String input = "Life is full of fun";
+        int vowelCount = countVowels(input);
+        System.out.println("Number of vowels: " + vowelCount);
+    }
+ public static int countVowels(String str) {
+        int count = 0;
+        str = str.toLowerCase(); 
+   for (int i = 0; i < str.length(); i++) {
+        char ch = str.charAt(i);
+         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+         count++;
+      }
+   }
+    return count;
+ }
+}
+Output:
+  Number of vowels:6
