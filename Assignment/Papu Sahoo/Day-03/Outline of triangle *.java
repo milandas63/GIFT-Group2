5f7b0b47a2822e425c89355b9759039c131b985d
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-14 08:02:06 +0530

    Update Outline of triangle *.java

--- Assignment/Papu Sahoo/Day-03/Outline of triangle *.java
+++ Assignment/Papu Sahoo/Day-03/Outline of triangle *.java
@@ -20,8 +20,8 @@ public class Pyramidoutline {
 	}
 }
 Output:
-
-              *
+					   
+                                                 *
 					        * *
 					       *   *
 					      *     *
