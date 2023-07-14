b06abf4e62b9e964ed7d6c9d3e706c2c6cb8eaeb
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-14 07:58:14 +0530

    Update Reverse every word.java

--- Assignment/Papu Sahoo/Day-05/Reverse every word.java
+++ Assignment/Papu Sahoo/Day-05/Reverse every word.java
@@ -1,1 +1,22 @@
+  // Write a program to reverse every word within a String?
+	  Example: Gandhi Institute For Technology //
+	  
+public class ReserveString {
+	public static void main(String[] args) {
+		text = "Gandhi Institute For Technology";
+		reverse = "";
+		words = text.split(" ");
+		for(int i=0; i<words.length; i++) {
+			alphas = words[i].toCharArray();
+			for(int j=alphas.length-1; j>=0; j--) {
+				reverse = reverse + alphas[j];
+			}
+			reverse = reverse + " ";
+		}
+		System.out.println("Original: "+text);
+		System.out.println("Reverse:  "+reverse);
+	}
+}
 
+Output:
+         ihdnaG etutitsnI roF ygolonhceT
