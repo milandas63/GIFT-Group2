93632e23289b6f9901105c163f7380f8747eb663
Milan Das <45796126+milandas63@users.noreply.github.com>
2023-07-15 11:48:23 +0530

    Add files via upload

---
+++ Milan Das/Day-07 (13-07-2023)/ExploreAPI.java
@@ -0,0 +1,26 @@
+package com.day7;
+
+import java.util.Scanner;
+
+import com.oops.Routine;
+
+public class ExploreAPI {
+	public static void main(String[] args) {
+		String words = "India";
+		String country = "India";
+		String x = "America";
+		String y = "India";
+		
+		Routine r = new Routine();
+		System.out.println(r.padL(23415164, 50));
+		System.out.println(r.padC(23415164, 50));
+		System.out.println(r.padR(23415164, 50));
+
+		
+		System.out.println("What is your name?");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		System.out.println(name);
+	}
+
+}
