51d4de3fff3a06f47d79f12c0ab7cf928a9c4abd
Kshitish Nanda <138613987+KSHI07@users.noreply.github.com>
2023-07-17 14:32:53 +0530

    Create Outline of triangle.java

---
+++ Assignment/Subhasish Mandal/Day3/Outline of triangle.java
@@ -0,0 +1,33 @@
+/* Draw a outline of a triangle using stars */
+
+public class Pyramidoutline {
+
+	public static void main(String[] args) {
+		int spaces = 40;
+		int stars = 1;
+		 for(int i=1; i<=10; i++) {
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=0; j<stars; j++) {
+				if(j>0 && j<(stars-1) && i<10) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+			spaces--; stars+=2;
+		}
+	}
+}
+Output:
+					   
+                                                 *
+					        * *
+					       *   *
+					      *     *
+					     *       *
+					    *         *
+					   *           *
+					  *             *
+					 *               *
+					*******************
