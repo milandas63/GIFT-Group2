f4c36ad3b481fd945c921e1205a468e199dc0ad5
Rashmi Ranjan Sethi <138992918+RASHMI-RANJAN-93@users.noreply.github.com>
2023-07-17 14:01:01 +0530

    Update asteriskstriangle.java

--- Assignment/Rashmi Ranjan Sethi/DAY-3(07-07-2023)/asteriskstriangle.java
+++ Assignment/Rashmi Ranjan Sethi/DAY-3(07-07-2023)/asteriskstriangle.java
@@ -1,1 +1,16 @@
+public class AsteriskTriangle {
+    public static void main(String[] args) {
+        int numRows = 5;
+        drawAsteriskTriangle(numRows);
+    }
+
+    public static void drawAsteriskTriangle(int numRows) {
+        for (int i = 1; i <= numRows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
 
