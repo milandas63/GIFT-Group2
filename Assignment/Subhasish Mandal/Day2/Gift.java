ff062fa16f7be9b19abeac1ef72031c98a188b13
Kshitish Nanda <138613987+KSHI07@users.noreply.github.com>
2023-07-17 14:31:34 +0530

    Create Gift.java

---
+++ Assignment/Subhasish Mandal/Day2/Gift.java
@@ -0,0 +1,19 @@
+pubic class Gift{
+  public static void main(string[] args) {
+    for(int i=0;i<=10;i++){
+      System.out.print("East or West our Gift,Autonomous is the best");
+      System.out.println();
+    }
+  }
+}
+output;
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
+East or West our Gift,Autonomous is the best
