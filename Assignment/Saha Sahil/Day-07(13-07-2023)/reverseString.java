2886b1818c49733a2223679afa27772106255bfe
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-18 10:08:30 +0530

    Update reverseString.java

--- Assignment/Saha Sahil/Day-07(13-07-2023)/reverseString.java
+++ Assignment/Saha Sahil/Day-07(13-07-2023)/reverseString.java
@@ -1,1 +1,20 @@
+import java.util.StringTokenizer;
 
+public class ReverseWords {
+
+    public static String reverseWords(String str) {
+        StringTokenizer st = new StringTokenizer(str, " ");
+        String reversedWords = "";
+        while (st.hasMoreTokens()) {
+            reversedWords += st.nextToken() + " ";
+        }
+        return reversedWords.trim();
+    }
+
+    public static void main(String[] args) {
+        String str = "God is Omnipotent and Omniscient";
+        System.out.println("Original string: " + str);
+        String reversedString = reverseWords(str);
+        System.out.println("Reversed string: " + reversedString);
+    }
+}
