9e3f9abb6477760957d2f01d730ac988e05e629a
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-18 22:07:30 +0530

    Update Pyramid.py

--- Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/Pyramid.py
+++ Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/Pyramid.py
@@ -1,1 +1,20 @@
+#
+#              *
+#			        ***
+#			       *****
+#			      *******
+#			     *********
+#			    ***********
+#			   *************
+#			  ***************
+# 		 *****************
 
+
+def print_pattern(rows):
+    for i in range(rows):
+        spaces = " " * (rows - i - 1)
+        stars = "*" * (2 * i + 1)
+        print(spaces + stars)
+
+rows = 9
+print_pattern(rows)
