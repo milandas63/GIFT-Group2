c8d6ea63219687aca7dbd983ccf4a5c8cad227c5
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-18 21:59:22 +0530

    Update Dimond.py

--- Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/Dimond.py
+++ Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/Dimond.py
@@ -1,5 +1,5 @@
-/*
-                  *        
+# Dimond shape pyramid
+                            ("                  *        
 					       ***       
 					      *****      
 					     *******     
@@ -14,7 +14,20 @@
 					    *********    
 					     *******     
 					      *****      
-					       *** 
-                  *    
-*/
-					        
+					       ***       
+					        *     
+
+
+def print_pattern(rows):
+    for i in range(1, rows + 1):
+        spaces = " " * (rows - i)
+        stars = "*" * (2 * i - 1)
+        print(spaces + stars)
+
+    for i in range(rows - 1, 0, -1):
+        spaces = " " * (rows - i)
+        stars = "*" * (2 * i - 1)
+        print(spaces + stars)
+
+rows = 10
+print_pattern(rows)
