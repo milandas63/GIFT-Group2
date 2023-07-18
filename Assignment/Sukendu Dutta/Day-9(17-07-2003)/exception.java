8883bcaf7ddf32b2f0d7f2e12a0e8c18abb68149
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-18 09:57:51 +0530

    Update exception.java

--- Assignment/Sukendu Dutta/Day-9(17-07-2003)/exception.java
+++ Assignment/Sukendu Dutta/Day-9(17-07-2003)/exception.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
 
+public class UsernamePasswordValidator {
+
+    private static final String[] users = {
+        {"Teacher", "milan"},
+        {"Gift", "college"},
+        {"Language", "java"},
+        {"Animal", "tiger"},
+        {"Student", "gift"},
+        {"Session", "second"}
+    };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        boolean isAuthenticated = false;
+
+        for (String[] user : users) {
+            if (user[0].equals(username) && user[1].equals(password)) {
+                isAuthenticated = true;
+                break;
+            }
+        }
+
+        if (isAuthenticated) {
+            System.out.println("Authentication successful!");
+        } else {
+            System.out.println("Authentication failed!");
+        }
+    }
+}
