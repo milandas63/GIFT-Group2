8c3a36356da09fa75cd0ee0a312100f31f204609
Rashmi Ranjan Sethi <138992918+RASHMI-RANJAN-93@users.noreply.github.com>
2023-07-19 13:51:26 +0530

    Create Dimondstyle.py

---
+++ Assignment/Rashmi Ranjan Sethi/Day-10(python)-(18-07-23)/Dimondstyle.py
@@ -0,0 +1,33 @@
+# Dimond shape pyramid
+#                 *        
+		#			       ***       
+		#			      *****      
+			#		     *******     
+			#		    *********    
+			#		   ***********   
+		#			  *************  
+		#			 *************** 
+		#			*****************
+		#			 *************** 
+		#			  *************  
+		#			   ***********   
+		#			    *********    
+		#			     *******     
+		#			      *****      
+		#			       ***       
+		#			        *     
+
+
+def print_pattern(rows):
+    for i in range(1, rows + 1):
+        spaces = " " * (rows - i)
+        stars = "*" * (2 * i - 1)
+        print(spaces + stars)
+
+    for i in range(rows - 1, 0, -1):
+        spaces = " " * (rows - i)
+        stars = "*" * (2 * i - 1)
+        print(spaces + stars)
+
+rows = 10
+print_pattern(rows)
