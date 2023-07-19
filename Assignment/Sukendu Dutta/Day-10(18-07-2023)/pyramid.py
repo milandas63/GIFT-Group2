c0b3e30267b62d43135987c20bf6308aac90aa62
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 14:01:38 +0530

    Update pyramid.py

--- Assignment/Sukendu Dutta/Day-10(18-07-2023)/pyramid.py
+++ Assignment/Sukendu Dutta/Day-10(18-07-2023)/pyramid.py
@@ -1,1 +1,6 @@
+def draw_pyramid(rows):
+    for i in range(rows):
+        print(' ' * (rows - i - 1) + '*' * (2 * i + 1))
+num_rows = int(input("Enter the number of rows for the pyramid: "))
+draw_pyramid(num_rows)
 
