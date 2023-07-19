56fe741d1496d3a925f7e80ebe72367c49a67143
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 21:10:17 +0530

    Update flip.java

--- Assignment/Rashmi Ranjan Sethi/Day-6/flip.java
+++ Assignment/Rashmi Ranjan Sethi/Day-6/flip.java
@@ -1,1 +1,13 @@
+public class FlipCharacters {
+    public static void main(String[] args) {
+        String input = "Hello, World!";
+        String flippedString = flipCharacters(input);
+        System.out.println(flippedString);
+    }
+
+    public static String flipCharacters(String input) {
+        StringBuilder flippedString = new StringBuilder(input);
+        return flippedString.reverse().toString();
+    }
+}
 
