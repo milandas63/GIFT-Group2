4d764f5e8ce5b82a982f4d1907800984e57309a0
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 21:05:56 +0530

    Update withinWord.java

--- Assignment/Rashmi Ranjan Sethi/Day-5(11-07-2023)/withinWord.java
+++ Assignment/Rashmi Ranjan Sethi/Day-5(11-07-2023)/withinWord.java
@@ -1,1 +1,20 @@
+public class ReverseWords {
+    public static void main(String[] args) {
+        String input = "Hello World! OpenAI is amazing.";
+        String reversedString = reverseWords(input);
+        System.out.println(reversedString);
+    }
+
+    public static String reverseWords(String input) {
+        String[] words = input.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word);
+            reversedString.append(reversedWord.reverse()).append(" ");
+        }
+
+        return reversedString.toString().trim();
+    }
+}
 
