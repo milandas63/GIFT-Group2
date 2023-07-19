ba2a1705d7c1bea7f38811d7ef12daf9de8144db
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 21:18:16 +0530

    Update string.java

--- Assignment/Rashmi Ranjan Sethi/Day-7/string.java
+++ Assignment/Rashmi Ranjan Sethi/Day-7/string.java
@@ -1,1 +1,21 @@
+public class ReverseWordsWithinString {
+    public static void main(String[] args) {
+        String input = "Hello World! OpenAI is amazing.";
+        String reversedString = reverseWordsWithinString(input);
+        System.out.println(reversedString);
+    }
+
+    public static String reverseWordsWithinString(String input) {
+        StringBuilder reversedString = new StringBuilder();
+        String[] words = input.split(" ");
+
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word);
+            reversedWord.reverse();
+            reversedString.append(reversedWord).append(" ");
+        }
+
+        return reversedString.toString().trim();
+    }
+}
 
