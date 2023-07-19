a91e2a98b3f6f0cea8f0f97f6ce7db11dc088981
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 21:08:20 +0530

    Update vowels.java

--- Assignment/Rashmi Ranjan Sethi/Day-6/vowels.java
+++ Assignment/Rashmi Ranjan Sethi/Day-6/vowels.java
@@ -1,1 +1,23 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String input = "Hello World! OpenAI is amazing.";
+        int vowelCount = countVowels(input);
+        System.out.println("Number of vowels: " + vowelCount);
+    }
+
+    public static int countVowels(String input) {
+        int count = 0;
+        input = input.toLowerCase(); 
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
 
