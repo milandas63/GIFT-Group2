a5548974fa399bfa2f80c9a0718ff2274fae32f1
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-19 22:18:58 +0530

    Update Reverse Word.py

--- Assignment/Papu Sahoo/Day-011/Reverse Word.py
+++ Assignment/Papu Sahoo/Day-011/Reverse Word.py
@@ -1,13 +1,2 @@
-//  Write a program to reverse all words in a string //
-def reverse_string(input_string):
-    return input_string[::-1]
+//  //
 
-if __name__ == "__main__":
-    input_string = "Gandhi Institute For Technology"
-    reversed_string = reverse_string(input_string)
-    print("Original String:", input_string)
-    print("Reversed String:", reversed_string)
-    
-Output:
-Original String: Gandhi Institute For Technology
-Reversed String: ygolonhceT roF etutitsnI ihdnaG
