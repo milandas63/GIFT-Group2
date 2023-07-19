a6acb5c888d870135ee8bb5d2508dc23e05687e1
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 21:37:26 +0530

    Update reverseWord.py

--- Assignment/Rashmi Ranjan Sethi/Day-11/reverseWord.py
+++ Assignment/Rashmi Ranjan Sethi/Day-11/reverseWord.py
@@ -1,1 +1,9 @@
+def reverse_words(input_string):
+    words = input_string.split(" ")
+    reversed_words = [word[::-1] for word in words]
+    return " ".join(reversed_words)
+
+input_string = "Hello World! "
+reversed_string = reverse_words(input_string)
+print(reversed_string)
 
