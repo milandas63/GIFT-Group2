b522a8c6015c3389e3b44f237f529bd1ef19558b
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 21:16:22 +0530

    Update palin.java

--- Assignment/Rashmi Ranjan Sethi/Day-7/palin.java
+++ Assignment/Rashmi Ranjan Sethi/Day-7/palin.java
@@ -1,1 +1,19 @@
+public class ReverseWords {
+    public static void main(String[] args) {
+        String input = "Hello World! OpenAI is amazing.";
+        String reversedString = reverseWords(input);
+        System.out.println(reversedString);
+    }
+
+    public static String reverseWords(String input) {
+        String[] words = input.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversedString.append(words[i]).append(" ");
+        }
+
+        return reversedString.toString().trim();
+    }
+}
 
