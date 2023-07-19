58bbfebeb1c574bfec1909364fdf076063863384
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-19 20:56:52 +0530

    Update reverseString.java

--- Assignment/Rashmi Ranjan Sethi/Day-5(11-07-2023)/reverseString.java
+++ Assignment/Rashmi Ranjan Sethi/Day-5(11-07-2023)/reverseString.java
@@ -1,1 +1,25 @@
+public class PalindromePyramid {
+    public static void main(String[] args) {
+        int height = 5; 
+
+        for (int i = 1; i <= height; i++) {
+            // Print spaces
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print("  ");
+            }
+
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (j + 64) + " ");
+            }
+
+            
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print((char) (j + 64) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
 
