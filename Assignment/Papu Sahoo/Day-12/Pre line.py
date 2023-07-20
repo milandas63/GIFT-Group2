7b59e042249b7c4c37c4c5ed2eee488b6bed28f5
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-20 22:53:38 +0530

    Update Pre line.py

--- Assignment/Papu Sahoo/Day-12/Pre line.py
+++ Assignment/Papu Sahoo/Day-12/Pre line.py
@@ -1,1 +1,24 @@
+"""
+Write a function that takes a list of strings and prints them, one per line, in a 
+	rectangular frame.
+    For example the list ["Hello", "World", "in", "a", "frame"]
+"""
+def print_in_frame(strings):
+    max_length = max(len(string) for string in strings)
+    print('*' * (max_length + 4))
+    for string in strings:
+        spaces = max_length - len(string)
+        print('* ' + string + ' ' * spaces + ' *')
+    print('*' * (max_length + 4))
+words = ["Hello", "World", "in", "a", "frame"]
+print_in_frame(words)
 
+Output:
+
+********
+*Hello *
+*World *
+*in    *
+*a     *
+*frame *
+********
