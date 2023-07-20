d7c6b086fb1a18d828554ff0c584f0f2eca345ac
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-20 19:58:06 +0530

    Update NumberPyramid.py

--- Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/NumberPyramid.py
+++ Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/NumberPyramid.py
@@ -1,4 +1,4 @@
- """                        1        
+ """                                            1        
 					       121       
 					      12321      
 					     1234321     
@@ -8,6 +8,21 @@
 					 123456787654321 
 					12345678987654321
 """
+
+
+
+spaces = 50
+    for i in range(1, 10):
+            for j in range(0, spaces):
+                print(" ", end="")
+            for j in range(1, i + 1):
+                print(j, end="")
+            for j in range(i - 1, 0, -1):
+                print(j, end="")
+
+
+                 or
+
 
 def print_pattern(rows):
     for i in range(1, rows + 1):
