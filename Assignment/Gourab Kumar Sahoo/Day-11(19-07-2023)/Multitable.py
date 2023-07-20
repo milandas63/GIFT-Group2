df93fd449140b0273603513d9f22ce94bb53a9f1
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-20 20:26:47 +0530

    Update Multitable.py

--- Assignment/Gourab Kumar Sahoo/Day-11(19-07-2023)/Multitable.py
+++ Assignment/Gourab Kumar Sahoo/Day-11(19-07-2023)/Multitable.py
@@ -1,3 +1,21 @@
+# Mutiplication Tables program
+start = 2
+end = 10
+for i in range(2, end+1, 5):
+	for j in range(1, 11):
+		for k in range(i, i+5):
+			if k>end:
+				break
+			# print(padL(k,2),"x",padL(j,2),"=",padL((k*j),3)," ", end='')
+			print(str(k).rjust(2,' '),"x",str(j).rjust(2,' '),"=",str(k*j).rjust(3,' ')," ", end='')
+		print()
+	print()
+
+print()
+
+
+print("-----------------------------------------x----------------------------")
+
 """
 	WAP to print all mutiplication tables between 2 and 25. Five tables to be printed
 	in a row.
