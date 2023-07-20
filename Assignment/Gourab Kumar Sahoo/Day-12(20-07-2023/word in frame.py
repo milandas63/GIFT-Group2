6b3f379645a2004b0fb8dcb29f0a3fd31174ffe5
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-20 20:47:12 +0530

    Update word in frame.py

--- Assignment/Gourab Kumar Sahoo/Day-12(20-07-2023/word in frame.py
+++ Assignment/Gourab Kumar Sahoo/Day-12(20-07-2023/word in frame.py
@@ -1,3 +1,16 @@
+""" Write a function that takes a list of strings and prints them, one per line, in a 
+	rectangular frame.
+    For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
+        *********
+        * Hello *
+        * World *
+        * in    *
+        * a     *
+        * frame *
+        *********
+"""
+
+
 def print_in_frame(strings):
     max_length = max(len(s) for s in strings)
     frame_width = max_length + 4  # Add 4 for the two stars and two spaces on each side
