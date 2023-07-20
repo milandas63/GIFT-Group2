7ff34b15296d283d3f7fcc52db1b1e33682a4a19
Srbidhar <138988065+Srbidhar@users.noreply.github.com>
2023-07-20 14:11:53 +0530

    Update reverseWords.py

--- Assignment/Soumya Ranjan Bidhar/Day-11(19-07-2023)/reverseWords.py
+++ Assignment/Soumya Ranjan Bidhar/Day-11(19-07-2023)/reverseWords.py
@@ -1,6 +1,6 @@
 def reverse_string(input_string):
     return input_string[::-1]
 
-input_string = "Hello World!"
+input_string = "quick brown fox jumps over the lazy dog!"
 reversed_string = reverse_string(input_string)
 print(reversed_string)
