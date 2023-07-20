66518e3c4ea5228e183baca6a5a4fdfd00819fd2
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-20 23:15:47 +0530

    Update Pyramid-3.py

--- Assignment/Papu Sahoo/Day-12/Pyramid-3.py
+++ Assignment/Papu Sahoo/Day-12/Pyramid-3.py
@@ -1,6 +1,21 @@
 """
      WAP to draw a triangle of palindrome numbers
 """
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+def draw_palindrome_triangle(rows):
+    current_row = 1
+    num = 1
+    while current_row <= rows
+        for _ in range(current_row):
+            while not is_palindrome(num):
+                num += 1
+            line += str(num).rjust(5)
+            num += 1
+        print(line)
+        current_row += 1
+
+Output:
               1             
              121                  
             12321                 
