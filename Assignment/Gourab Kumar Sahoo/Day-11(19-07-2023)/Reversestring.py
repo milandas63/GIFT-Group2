17a7a8ef136d66180307285cd4bacdb92ed7bcb8
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-20 20:05:37 +0530

    Create Reversestring.py

---
+++ Assignment/Gourab Kumar Sahoo/Day-11(19-07-2023)/Reversestring.py
@@ -0,0 +1,28 @@
+
+"""
+	WAP to reverse a string
+		Original text: Quick Brown Fox Jumps Over The Lazy Dog
+		Reverse text:  goD yzaL ehT revO spmuJ xoF nworB kciuQ
+"""
+original = "Quick Brown Fox Jumps Over The Lazy Dog"
+reverse = ""
+
+for i in range(len(original)-1, -1, -1):
+	reverse = reverse+original[i]
+
+print("Original text: "+original)
+print("Reverse text:  "+reverse)
+
+print()
+print("------------------------------ Next --------------------------------")
+
+"""
+	WAP to print alternate characters in the string
+		Quick Brown Fox Jumps Over The Lazy Dog
+		QikBonFxJmsOe h ayDg
+"""
+original = "Quick Brown Fox Jumps Over The Lazy Dog"
+for i in range(0, len(original), 2):
+	print(original[i], end='')
+
+print()
