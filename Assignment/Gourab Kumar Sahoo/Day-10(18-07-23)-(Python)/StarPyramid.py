d22feefe84bc885f8a6c1b1378d492ea634d11e7
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-20 19:28:14 +0530

    Update StarPyramid.py

--- Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/StarPyramid.py
+++ Assignment/Gourab Kumar Sahoo/Day-10(18-07-23)-(Python)/StarPyramid.py
@@ -1,14 +1,28 @@
-#
-#                    *
-#			        ***
-#			       *****
-#			      *******
-#			     *********
-#			    ***********
-#			   *************
-#			  ***************
-# 		     *****************
+"""
+                     *
+			        ***
+			       *****
+			      *******
+			     *********
+			    ***********
+			   *************
+			  ***************
+ 		     *****************
+"""
 
+
+spaces = 50
+stars = 1
+for i in range(1, 10):
+	for j in range(0, spaces):
+		print(" ", end="")
+	for j in range(0, stars):
+		print("*", end="")
+	print()
+	spaces = spaces-1
+	stars = stars+2            
+
+       or
 
 def print_pattern(rows):
     for i in range(rows):
