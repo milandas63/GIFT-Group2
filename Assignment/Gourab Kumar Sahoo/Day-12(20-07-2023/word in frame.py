a15af26ad96efc8339875179f03ef6f60506a928
Gourab451 <139215355+Gourab451@users.noreply.github.com>
2023-07-20 20:46:30 +0530

    Create word in frame.py

---
+++ Assignment/Gourab Kumar Sahoo/Day-12(20-07-2023/word in frame.py
@@ -0,0 +1,18 @@
+def print_in_frame(strings):
+    max_length = max(len(s) for s in strings)
+    frame_width = max_length + 4  # Add 4 for the two stars and two spaces on each side
+
+    # Print the top frame border
+    print("*" * frame_width)
+
+    # Print each string with padding to align in the frame
+    for s in strings:
+        padding = " " * (max_length - len(s))
+        print(f"* {s}{padding} *")
+
+    # Print the bottom frame border
+    print("*" * frame_width)
+
+# Example usage:
+words = ["Hello", "World", "in", "a", "frame"]
+print_in_frame(words)
