d60a49d011a3efc82c4b0271fe4f2afad0cf8624
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-21 09:48:36 +0530

    Update Pyramid-2.py

--- Assignment/Papu Sahoo/Day-12/Pyramid-2.py
+++ Assignment/Papu Sahoo/Day-12/Pyramid-2.py
@@ -1,6 +1,14 @@
 """
    WAP to draw a diamond style of asterisks
 """
+def draw_diamond(height):
+    if height % 2 == 0:
+        height += 1  
+    for i in range(1, height + 1, 2):
+        print(("*" * i).rjust(height)) 
+    for i in range(height - 2, 0, -2):
+        print(("*" * i).rjust(height))
+draw_diamond(16)
     
                     *           
                    ***                  
