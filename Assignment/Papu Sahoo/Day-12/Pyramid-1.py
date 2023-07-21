ca3d5e08d45d9b53ee7a3bcb340e3374fb8097f8
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-21 10:01:15 +0530

    Update Pyramid-1.py

--- Assignment/Papu Sahoo/Day-12/Pyramid-1.py
+++ Assignment/Papu Sahoo/Day-12/Pyramid-1.py
@@ -1,6 +1,12 @@
 """
  WAP to draw a triangle of asterisks
 """
+def draw_triangle(height):
+    for i in range(1, height + 1):
+        line = '*' * (2 * i - 1)
+        print(line.rjust(height + i - 1))
+height = 8
+draw_triangle(height)
    
               *           
              ***                
@@ -10,5 +16,3 @@
         *************         
        ***************        
       *****************      
-
-
