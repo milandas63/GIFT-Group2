5f37caaa60007d3651e6680d7e2a993ef125b182
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-21 20:40:03 +0530

    Update ReadCharecter.py

--- Assignment/Sukendu Dutta/Day-13(21-07-2023)/ReadCharecter.py
+++ Assignment/Sukendu Dutta/Day-13(21-07-2023)/ReadCharecter.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     input_text = input("Enter the text: ")
     print_with_spaces(input_text)
 
-Output=
+Output =
 Enter a string: Hello World!
 H e l l o   W o r l d !
 
