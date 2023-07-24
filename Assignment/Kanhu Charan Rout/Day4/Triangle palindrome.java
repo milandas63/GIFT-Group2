c7ae922f2735a57c3d860de322bd115c4b52a035
KANHUREPO <139123318+KANHUREPO@users.noreply.github.com>
2023-07-24 10:51:43 +0530

    Update Triangle palindrome.java

--- Assignment/Kanhu Charan Rout/Day4/Triangle palindrome.java
+++ Assignment/Kanhu Charan Rout/Day4/Triangle palindrome.java
@@ -1,1 +1,37 @@
 
+/* 
+ Q:- Draw a triangle of palindrome alphabets pyramid  */ 
+  
+ public class PalindromeTriangle { 
+     public static void main(String[] args) { 
+         int rows = 5; // Specify the number of rows for the pyramid 
+  
+         for (int i = 0; i < rows; i++) { 
+             // Print spaces for alignment 
+             for (int j = 0; j < rows - i; j++) { 
+                 System.out.print("  "); 
+             } 
+  
+             // Print alphabets in ascending order 
+             for (int j = 0; j <= i; j++) { 
+                 System.out.print((char)('A' + j) + " "); 
+             } 
+  
+             // Print alphabets in descending order 
+             for (int j = i - 1; j >= 0; j--) { 
+                 System.out.print((char)('A' + j) + " "); 
+             } 
+  
+             System.out.println(); // Move to the next line 
+         } 
+     } 
+ } 
+  
+ /* 
+ Output 
+ When you run this code, it will generate the following output: 
+  
+           A  
+         A B A  
+       A B C B A  
+     A B C D C B A 
