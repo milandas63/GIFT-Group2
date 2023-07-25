45ffacde7e776e0865991574a4c438c73e56aff3
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-25 09:59:27 +0530

    Update Reverse Word.py

--- Assignment/Papu Sahoo/Day-11/Reverse Word.py
+++ Assignment/Papu Sahoo/Day-11/Reverse Word.py
@@ -1,1 +1,14 @@
+"""
+Write a program to reverse the charecters of each word in a string 
+"""
+def reverse_words(input_string):
+   words = input_string.split()
+  reversed_words = words[::-1]
+  reversed_string = " ".join(reversed_words)
+    return reversed_string
+input_string = "God is Omnipotent and Omniscient"
+reversed_string = reverse_words(input_string)
+print(reversed_string)
 
+Output:
+Omniscient and Omnipotent is God
