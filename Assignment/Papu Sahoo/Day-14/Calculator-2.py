b61f39b7d6f5f0565767704ed8ef6810b4cc726b
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-25 09:07:17 +0530

    Update Calculator-2.py

--- Assignment/Papu Sahoo/Day-14/Calculator-2.py
+++ Assignment/Papu Sahoo/Day-14/Calculator-2.py
@@ -1,1 +1,27 @@
+"""
+Write a program to perform Age calculator ?
+"""
+from datetime import datetime
+
+def calculate_age(birthdate):
+    try:
+        birthdate = datetime.strptime(birthdate, "%Y-%m-%d")
+        current_date = datetime.now()
+        age = current_date.year - birthdate.year - ((current_date.month, current_date.day) < (birthdate.month, birthdate.day))
+        return age
+    except ValueError:
+        return "Invalid date format. Please use 'YYYY-MM-DD'."
+
+def main():
+    print("Welcome to the Age Calculator")
+    birthdate_input = input("Please enter your birthdate (YYYY-MM-DD): ")
+
+    age = calculate_age(birthdate_input)
+    if isinstance(age, int):
+        print(f"You are {age} years old.")
+    else:
+        print(age)
+
+if __name__ == "__main__":
+    main()
 
