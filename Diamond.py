ca303a7b24ac530066cca87e2e712366acbe96e3
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-25 14:50:55 +0530

    Create Diamond.py

---
+++ Diamond.py
@@ -0,0 +1,27 @@
+WAP to draw a diamond style of asterisks?
+  
+
+                                     *           
+                                    ***                
+                                   *****                 
+                                  *******            
+                                 *********             
+                                ***********           
+                               *************         
+                              *************** 
+                             *****************     
+                              ***************                         
+                               *************
+                                ***********
+                                 *********
+                                  *******
+                                   *****
+                                    ***
+                                     *
+def draw_diamond(rows):
+    for i in range(rows):
+        print(' ' * (rows - i - 1) + '*' * (2 * i + 1))
+    for i in range(rows - 2, -1, -1):
+        print(' ' * (rows - i - 1) + '*' * (2 * i + 1))
+num_rows = int(input("Enter the number of rows for the diamond: "))
+draw_diamond(num_rows)
