82085b9d08c174ac576da1c9dad19eab004aaffa
sonu784680 <139066206+sonu784680@users.noreply.github.com>
2023-07-26 15:06:56 +0530

    Create Age calc.py

---
+++ Assignment/Maheswar Malik/Day14/Age calc.py
@@ -0,0 +1,44 @@
+"""
+WAP TO DEIGNE A AGE CALCULATOR..
+"""
+def calculate_age(birth_year, current_year):
+    age = current_year - birth_year
+    return age
+
+def main():
+    more = True
+    while more:
+        print("\nAge Calculator")
+        print("--------------")
+        birth_year = int(input("Enter your birth year: "))
+        current_year = int(input("Enter the current year: "))
+
+        age = calculate_age(birth_year, current_year)
+
+        print("\nYour age is:", age)
+
+        more = input("\nMore [y/n]: ").lower() == 'y'
+
+if __name__ == "__main__":
+    main()
+
+
+
+OUTPUT:
+Age Calculator
+--------------
+Enter your birth year: 1995
+Enter the current year: 2023
+
+Your age is: 28
+
+More [y/n]: y
+
+Age Calculator
+--------------
+Enter your birth year: 2000
+Enter the current year: 2023
+
+Your age is: 23
+
+More [y/n]: n
