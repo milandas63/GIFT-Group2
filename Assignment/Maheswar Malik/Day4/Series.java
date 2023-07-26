9c8dc66611aba25424d9d8c517d65d8bc512d46f
sonu784680 <139066206+sonu784680@users.noreply.github.com>
2023-07-26 14:35:16 +0530

    Create Series.java

---
+++ Assignment/Maheswar Malik/Day4/Series.java
@@ -0,0 +1,22 @@
+/*
+Q:- Complete the series of 10 numbers in the series?
+        1, 3, 5, 11, 21, ......      (n, n*2+1, n*2-1, n*2+1, n*2-1, ...  */
+
+public class SeriesCompletion {
+    public static void main(String[] args) {
+        int n = 21; // Specify the last number in the given series
+        
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                n = n * 2 + 1;
+            } else {
+                n = n * 2 - 1;
+            }
+            System.out.print(n + " ");
+        }
+    }
+}
+/*
+Output
+When you run this code, it will output the next 10 numbers in the series
+43 85 171 341 683 1365 2731 5461 10923 21845 */
