a313af5a16f3c06c7e77056b4c4abab13b9fdd11
OMMBISWAJIT <138613561+OMMBISWAJIT@users.noreply.github.com>
2023-07-27 18:05:37 +0530

    Create idpasswoed.java

---
+++ Assignment/Omm Biswajit Mohanty/Day9/idpasswoed.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class UserValidation {
+    private static final String[][] users = {
+            {"Teacher", "milan"},
+            {"Gift", "college"},
+            {"Language", "java"},
+            {"Animal", "tiger"},
+            {"Student", "gift"},
+            {"Session", "second"}
+    };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine().trim();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine().trim();
+
+        try {
+            validateUser(username, password);
+            System.out.println("Login successful!");
+        } catch (UserException e) {
+            System.out.println("Login failed: " + e.getMessage());
+        }
+    }
+
+    private static void validateUser(String username, String password) throws UserException {
+        if (username.isEmpty()) {
+            throw new EmptyUsernameException("Username cannot be empty.");
+        }
+
+        if (password.isEmpty()) {
+            throw new EmptyPasswordException("Password cannot be empty.");
+        }
+
+        for (String[] user : users) {
+            String storedUsername = user[0];
+            String storedPassword = user[1];
+
+            if (username.equals(storedUsername)) {
+                if (password.equals(storedPassword)) {
+                    return; // Valid username and password
+                } else {
+                    throw new WrongPasswordException("Incorrect password.");
+                }
+            }
+        }
+
+        throw new WrongUsernameException("Username not found.");
+    }
+}
