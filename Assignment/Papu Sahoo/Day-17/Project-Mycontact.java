24ac205e4c6e81889c2232e7196d46ecfa71b127
Papu2023 <138614206+Papu2023@users.noreply.github.com>
2023-07-27 22:41:47 +0530

    Update Project-Mycontact.java

--- Assignment/Papu Sahoo/Day-17/Project-Mycontact.java
+++ Assignment/Papu Sahoo/Day-17/Project-Mycontact.java
@@ -43,7 +43,7 @@ public class CategoryDisplay {
 	}
 }
 
-// CATAGORY DISPLAY //
+// CATAGORY MENU //
 
 package com.contact.main;
 
@@ -210,4 +210,37 @@ INSERT INTO contact VALUES
 
 
 // CONTACT MENU //
+
+package com.contact.main;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class ContactMenu {
+	public ContactMenu(Connection conn, Scanner scan) {
+		String choice;
+		boolean yn = true;
+		while(yn) {
+			for(int i=0; i<10; i++) System.out.println();
+			System.out.println("Contact Menu");
+			System.out.println("1. Display");
+			System.out.println("2. Add New");
+			System.out.println("3. Update");
+			System.out.println("4. Delete");
+			System.out.println("5. Exit");
+			System.out.println();
+			System.out.print("Choice: ");
+			choice = scan.nextLine();
+			if(choice.equals("1")) {
+			} else if(choice.equals("2")) {
+			} else if(choice.equals("3")) {
+			} else if(choice.equals("4")) {
+			} else if(choice.equals("5")) {
+				yn = false;
+			}
+		}
+	}
+}
+
+// LOCATION MENU //
 
