f2fb8e5c0dc89e5e8476ca8790ecd14c20b83dc3
sonu784680 <139066206+sonu784680@users.noreply.github.com>
2023-07-27 10:06:56 +0530

    Create Pyramidfunction.py

---
+++ Assignment/Millan Kumar Patra/Day12/Pyramidfunction.py
@@ -0,0 +1,18 @@
+# write the pyramid program using rjust() function?
+
+
+
+def print_pyramid(rows):
+    for i in range(1, rows + 1):
+        # Calculate the number of spaces on the left side of the pyramid
+        spaces = (rows - i) * " "
+        
+        # Calculate the number of stars in each row
+        stars = (2 * i - 1) * "*"
+        
+        # Combine spaces and stars and print the row
+        print(spaces.rjust(rows + i - 1) + stars)
+
+# Example usage:
+num_rows = 5
+print_pyramid(num_rows)
