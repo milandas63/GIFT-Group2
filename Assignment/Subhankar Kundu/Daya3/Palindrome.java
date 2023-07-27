660de9b0474ea3442111ee2f3d19410e24015663
SUBHAMREPO <138994553+SUBHAMREPO@users.noreply.github.com>
2023-07-27 10:57:56 +0530

    Create Palindrome.java

---
+++ Assignment/Subhankar Kundu/Daya3/Palindrome.java
@@ -0,0 +1,15 @@
+public class Pyramidnumber {
+
+	public static void main(String[] args) {
+		int spaces = 40;
+		 for(int i=1; i<=9; i++) {
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=1; j<=i; j++)      System.out.print(j);
+			for(int j=(i-1); j>0; j--)   System.out.print(j);
+			System.out.println();
+			spaces--;
+		}
+	}
+
+}
+Output:
