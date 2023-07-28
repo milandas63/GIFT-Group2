d066de57f072b52d005e7dc809f053f5c39c5e72
Satya7848 <139062059+Satya7848@users.noreply.github.com>
2023-07-28 10:31:54 +0530

    Create Frame.py

---
+++ Assignment/Satyajeet Biswal/Frame.py
@@ -0,0 +1,19 @@
+def print_in_frame(strings):
+  width = len(max(strings, key=len))
+  height = len(strings)
+  print('*' * (width + 4))
+  for i in range(height):
+    print('* {:<{}} *'.format(strings[i], width))
+  print('*' * (width + 4))
+if _name_ == '_main_':
+  strings = ["Hello", "World", "in", "a", "frame"]
+  print_in_frame(strings)
+  """ OUTPUT
+         * * * * *
+         * Hello * 
+         * World * 
+         * in    * 
+         * a     * 
+         * frame * 
+         * * * * *
+         """
