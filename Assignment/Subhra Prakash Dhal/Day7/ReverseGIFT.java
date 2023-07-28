15d984180480d496a6dc8b038a652d51c8bd0ace
SubhraPrakashDhal <138770597+SubhraPrakashDhal@users.noreply.github.com>
2023-07-28 07:17:43 +0530

    Update ReverseGIFT.java

--- Assignment/Subhra Prakash Dhal/Day7/ReverseGIFT.java
+++ Assignment/Subhra Prakash Dhal/Day7/ReverseGIFT.java
@@ -1,1 +1,27 @@
 
+public class ReserveString { 
+         public static void main(String[] args) { 
+                 // Question-1 
+                 String text = "Gandhi Institute For Technology"; 
+                 String reverse = ""; 
+                 char[] alphas = text.toCharArray(); 
+                 for(int i=alphas.length-1; i>=0; i--) { 
+                         reverse = reverse+alphas[i]; 
+                 } 
+  
+                 System.out.println("Original: "+text); 
+                 System.out.println("Reverse:  "+reverse); 
+  
+                 System.out.println("----------------------------------------------------"); 
+  
+                 text = "God is Omnipotent and Omniscient"; 
+                 reverse = ""; 
+                 String[] words = text.split(" "); 
+                 for(int i=words.length-1; i>=0; i--) { 
+                         reverse = reverse + words[i]+ " "; 
+                 } 
+                 System.out.println("Original: "+text); 
+                 System.out.println("Reverse:  "+reverse); 
+  
+   } 
+ }
