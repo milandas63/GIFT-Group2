3588fd7a43d517ec4b15d894ac0fa4453efadc85
Sukhendu20 <138586049+Sukhendu20@users.noreply.github.com>
2023-07-28 22:43:05 +0530

    Create LocationMenu.java

---
+++ Assignment/Sukendu Dutta/day-16(26-07-2023)project-3/LocationMenu.java
@@ -0,0 +1,31 @@
+package com.location.main;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class LocationMenu {
+	public LocationMenu(Connection conn, Scanner scan) {
+		String choice;
+		boolean yn = true;
+		while(yn) {
+			for(int i=0; i<10; i++) System.out.println();
+			System.out.println("Location Menu");
+			System.out.println("1. Display");
+			System.out.println("2. Add New");
+			System.out.println("3. Update");
+			System.out.println("4. Delete");
+			System.out.println("5. Exit");
+			System.out.println();
+			System.out.print("Choice: ");
+			choice = scan.nextLine();
+			if(choice.equals("1")) {
+			} else if(choice.equals("2")) {
+			} else if(choice.equals("3")) {
+			} else if(choice.equals("4")) {
+			} else if(choice.equals("5")) {
+				yn = false;
+			}
+		}
+
+	}
+}
