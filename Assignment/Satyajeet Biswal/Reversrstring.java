d3ea0aaeb1330f74593921bccf9d7b61d3ce24b5
Satya7848 <139062059+Satya7848@users.noreply.github.com>
2023-07-28 10:17:57 +0530

    Create Reversrstring.java

---
+++ Assignment/Satyajeet Biswal/Reversrstring.java
@@ -0,0 +1,25 @@
+import java.util.StringTokenizer;
+
+public class ReverseWords {
+
+    public static String reverseWords(String str) {
+        StringTokenizer st = new StringTokenizer(str, " ");
+        String reversedWords = "";
+        while (st.hasMoreTokens()) {
+            String word = st.nextToken();
+            String reversedWord = "";
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reversedWord += word.charAt(i);
+            }
+            reversedWords += reversedWord + " ";
+        }
+        return reversedWords.trim();
+    }
+
+    public static void main(String[] args) {
+        String str = "Gandhi Institute For Technology";
+        System.out.println("Original string: " + str);
+        String reversedString = reverseWords(str);
+        System.out.println("Reversed string: " + reversedString);
+    }
+}
