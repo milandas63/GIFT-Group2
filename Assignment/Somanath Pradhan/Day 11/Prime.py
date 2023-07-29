57bba744c76e9a698f025e433e8f2290631c2e23
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-29 08:36:59 +0530

    Update Prime.py

--- Assignment/Somanath Pradhan/Day 11/Prime.py
+++ Assignment/Somanath Pradhan/Day 11/Prime.py
@@ -1,1 +1,15 @@
-
+def is_prime(number): 
+     if number < 2: 
+         return False 
+     for i in range(2, int(number ** 0.5) + 1): 
+         if number % i == 0: 
+             return False 
+     return True 
+  
+ lower_limit = 1000 
+ upper_limit = 3000 
+  
+ print("Prime numbers between", lower_limit, "and", upper_limit, "are:") 
+ for number in range(lower_limit, upper_limit + 1): 
+     if is_prime(number): 
+         print(number)
