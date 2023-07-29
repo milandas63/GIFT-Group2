eb88bc48db1c6b2d6fede2a54901d06cd959bd20
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-29 08:40:20 +0530

    Update ReadCharecterOneSpace.py

--- Assignment/Somanath Pradhan/Day 13/ReadCharecterOneSpace.py
+++ Assignment/Somanath Pradhan/Day 13/ReadCharecterOneSpace.py
@@ -1,1 +1,11 @@
-
+def print_with_spaces(text): 
+     for char in text: 
+         print(char, end=" ") 
+  
+ if __name__ == "__main__": 
+     input_text = input("Enter the text: ") 
+     print_with_spaces(input_text) 
+  
+ Output = 
+ Enter a string: Hello World! 
+ H e l l o   W o r l d !
