f0484c33add371728ee925e63444dc9404b296e5
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-29 08:40:50 +0530

    Update ExaminationMarking .py

--- Assignment/Somanath Pradhan/Day 13/ExaminationMarking .py
+++ Assignment/Somanath Pradhan/Day 13/ExaminationMarking .py
@@ -1,1 +1,8 @@
-
+calculate_grace_marks = lambda aggregate, num_failed_subjects: 10 if aggregate == "pass" and num_failed_subjects == 1 else (8 if aggregate == "pass" and num_failed_subjects == 2 else (6 if aggregate == "pass" and num_failed_subjects == 3 else 0)) 
+  
+ if __name__ == "__main__": 
+     # Test cases 
+     print(calculate_grace_marks("pass", 1))  # Output: 10 
+     print(calculate_grace_marks("pass", 2))  # Output: 8 
+     print(calculate_grace_marks("pass", 3))  # Output: 6 
+     print(calculate_grace_marks("fail", 2))  # Output: 0
