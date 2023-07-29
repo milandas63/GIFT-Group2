2d6a9d96c60b47473f9910c00c80c878f3722513
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-29 08:34:40 +0530

    Update PalinPyramid.java

--- Assignment/Somanath Pradhan/Day 10/PalinPyramid.java
+++ Assignment/Somanath Pradhan/Day 10/PalinPyramid.java
@@ -1,1 +1,10 @@
-
+def draw_palindrome_pyramid(rows): 
+     for i in range(1, rows + 1): 
+         for j in range(1, i + 1): 
+             print(j, end=' ') 
+         for j in range(i - 1, 0, -1): 
+             print(j, end=' ') 
+  
+         print() 
+ num_rows = int(input("Enter the number of rows for the palindrome pyramid: ")) 
+ draw_palindrome_pyramid(num_rows)
