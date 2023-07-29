de460f0304c2c60732aa0c7ae6e0a2a494a15bd5
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-29 08:36:11 +0530

    Update ReverseChar.py

--- Assignment/Somanath Pradhan/Day 11/ReverseChar.py
+++ Assignment/Somanath Pradhan/Day 11/ReverseChar.py
@@ -1,1 +1,6 @@
-
+def reverse_string(input_string): 
+     return input_string[::-1] 
+  
+ input_string = "Hello World! " 
+ reversed_string = reverse_string(input_string) 
+ print(reversed_string)
