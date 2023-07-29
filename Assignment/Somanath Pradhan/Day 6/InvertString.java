a49818b41b3d1c531d1b8316b857d05f0accb50f
somanathpradhan09 <138600218+somanathpradhan09@users.noreply.github.com>
2023-07-29 08:28:34 +0530

    Update InvertString.java

--- Assignment/Somanath Pradhan/Day 6/InvertString.java
+++ Assignment/Somanath Pradhan/Day 6/InvertString.java
@@ -1,1 +1,26 @@
-
+public class CaseFlip { 
+     public static void main(String[] args) { 
+         String original = "Quick Brown Fox Jumps Over The Lazy Dog"; 
+         String inverted = flipCase(original); 
+         System.out.println("Original: " + original); 
+         System.out.println("Inverted: " + inverted); 
+     } 
+  
+     public static String flipCase(String str) { 
+         StringBuilder result = new StringBuilder(); 
+  
+         for (int i = 0; i < str.length(); i++) { 
+             char ch = str.charAt(i); 
+  
+             if (Character.isUpperCase(ch)) { 
+                 result.append(Character.toLowerCase(ch)); 
+             } else if (Character.isLowerCase(ch)) { 
+                 result.append(Character.toUpperCase(ch)); 
+             } else { 
+                 result.append(ch); 
+             } 
+         } 
+  
+         return result.toString(); 
+     } 
+}
